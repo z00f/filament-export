1dd90af208e4d333963b65bf6e100270a810cd47
z00f <34635749+z00f@users.noreply.github.com>
2022-09-08 08:10:05 -0600

    Update FilamentReportServiceProvider.php

--- src/FilamentReportServiceProvider.php
+++ src/FilamentReportServiceProvider.php
@@ -14,21 +14,21 @@ class FilamentReportServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-export');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-report');
 
-        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'filament-export');
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'filament-report');
 
         $this->publishes([
             __DIR__ . '/../config/filament-report.php' => config_path('filament-report.php'),
         ], 'config');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/filament-export'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/filament-report'),
         ], 'views');
 
         Filament::serving(function () {
-            Filament::registerScripts([__DIR__ . '/../resources/js/filament-export.js']);
-            Filament::registerStyles([__DIR__ . '/../resources/css/filament-export.css']);
+            Filament::registerScripts([__DIR__ . '/../resources/js/filament-report.js']);
+            Filament::registerStyles([__DIR__ . '/../resources/css/filament-report.css']);
         });
     }
 }
